de134eb3c92b637f0b967b79f2a3cbcf3492dbe7
Pedro Gutiérrez <pedrogutierrez@mac.com>
2023-08-15 22:30:27 +0200

    Release v0.0.3

--- installer/mix.exs
+++ installer/mix.exs
@@ -4,7 +4,7 @@ defmodule Sleeky.New.MixProject do
   def project do
     [
       app: :sleeky_new,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
