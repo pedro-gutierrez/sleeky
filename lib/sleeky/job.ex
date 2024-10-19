b339c62160e000fe66862926307d7fd50d59192e
Pedro Gutiérrez <pedrogutierrez@mac.com>
2024-10-19 19:47:44 +0200

    Simplify scheduled jobs code

--- lib/sleeky/job.ex
+++ lib/sleeky/job.ex
@@ -2,6 +2,7 @@ defmodule Sleeky.Job do
   @moduledoc """
   Generates function to schedule background jobs on different model actions
   """
+  use Oban.Worker, queue: :default
 
   alias Sleeky.Job.Worker
 
@@ -10,12 +11,18 @@ defmodule Sleeky.Job do
   def schedule_all(model, action, jobs) do
     with [_ | _] <-
            jobs
-           |> Enum.map(&worker(model, action, &1))
+           |> Enum.map(&new(%{model: model.__struct__, id: model.id, action: action, job: &1}))
            |> Oban.insert_all(),
          do: :ok
   end
 
-  defp worker(model, action, job) do
-    Worker.new(%{model: model.__struct__, id: model.id, action: action, job: job})
+  @impl Oban.Worker
+  def perform(%Oban.Job{args: %{"model" => model, "id" => id, "job" => job}}) do
+    model = Module.concat([model])
+    job = Module.concat([job])
+
+    with {:ok, model} <- model.fetch(id) do
+      job.execute(model)
+    end
   end
 end
